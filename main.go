5f44da081108457013771fe1aee4d3232e3e2dea
golden <120225883@qq.com>
2021-10-22 14:49:11 +0800

    add version

--- main.go
+++ main.go
@@ -6,13 +6,32 @@ import (
 	"log"
 )
 
+var (
+	v bool
+)
+
+const (
+	Version = "2.0"
+	Sha     = ""
+)
+
+func printV() {
+	println("Version: ", Version)
+	println("Sha: ", Sha)
+}
+
 func main() {
 	opt := clock.NewOptions()
 	flag.StringVar(&opt.Start, "start", "09:00", "上班时间")
 	flag.StringVar(&opt.End, "end", "18:00", "下班时间")
 	flag.StringVar(&opt.Adb, "adb", "/usr/local/bin/adb", "adb 可执行文件路径")
 	flag.StringVar(&opt.Passwd, "password", "", "解锁密码")
+	flag.BoolVar(&v, "v", false, "显示版本号")
 	flag.Parse() // 解析参数
+	if v {
+		printV()
+		return
+	}
 	log.Println("你的上班时间为: ", opt.Start)
 	log.Println("你的下班时间为: ", opt.End)
 	log.Println("你的adb路径为: ", opt.Adb)
